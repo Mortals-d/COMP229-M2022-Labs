43b571e10fc5d938c53c07b0b329e1447a53de76
Mortals-d <dhb061@gmail.com>
2023-06-02 16:51:30 -0400

    completed basic site structure

---
+++ TTwk3/.history/server_20230531224531.ts
@@ -0,0 +1,44 @@
+//ctrl+shift+b to compile and run the watch mode
+//require built-in modules
+const http = require('http');//CommonJS Module pattern(CJS)
+const fs = require('fs');
+const mime = require('mime-types');
+
+let lookup = mime.lookup;//alias for mime.lookup
+
+const port = 3000;
+
+//when we create a server instance, it's IMMUTABLE = cannot be changed
+const server = http.createServer(function(req, res)
+{
+    let path = req.url;//alias fro req.url
+    console.log(`Req url: ${req.url}`);
+    if(path == "/" || path =="/home")
+    {
+        path = "/index.html";
+    }
+
+    let mime_type = lookup(path.substring(1));
+    console.log(`MIME TYPE: ${mime_type}`);
+
+
+    fs.readFile(__dirname + path, function(err, data)
+    {        
+        if(err)
+        {
+            res.writeHead(404);//status - file not found 
+            console.log(`ERROR: ${err.message}`);//output to the dev console
+            return res.end("ERROR 404");
+            //return res.end(err.message);//output the error message to the page
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");//security guard
+        res.writeHead(200, {'Content-Type': mime_type});//status - all ok
+        //console.log(`DATA: ${data}`);
+        return res.end(data);//output the file that was read to the page
+    });
+});
+
+server.listen(port, function() //server.addEventListener("req")
+{
+    console.log(`Server running at Port: ${port}`);//template string
+});
---
+++ TTwk3/.history/server_20230602104711.ts
@@ -0,0 +1,17 @@
+//ctrl+shift+b to compile and run the watch mode
+//require built-in modules
+import express from 'express';
+
+const app = express(); //create a new express application
+
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//listener - similar to server.listen
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
