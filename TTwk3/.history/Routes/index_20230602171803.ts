18ed861031acb2c01441ae2dc80af71169edbeef
Mortals-d <dhb061@gmail.com>
2023-06-02 17:21:46 -0400

    converted site to typescript

---
+++ TTwk3/.history/Routes/index_20230602171803.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req:express.Request, res, next) 
+{
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
